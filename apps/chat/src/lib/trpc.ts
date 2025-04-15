1502e2568cb64e47c1a98a95562624a68653f849
Michael <michaeljohneduave@gmail.com>
2025-04-15 16:29:21 -0700

    Fix incorrect import

--- apps/chat/src/lib/trpc.ts
+++ apps/chat/src/lib/trpc.ts
@@ -1,5 +1,5 @@
 import { useSession } from "@clerk/vue";
-import type { AppRouter } from "@hugin-bot/functions/src/trpc";
+import type { AppRouter } from "@hugin-bot/functions/src/lib/trpc";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 
 export const useTrpc = () => {
