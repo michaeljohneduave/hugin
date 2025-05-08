b0824f879ef01344fc5b303ebfd8cad14bf300b8
Michael Eduave <michaeljohneduave@gmail.com>
2025-05-07 17:11:30 -0700

    Hard set timezone to PST Add web search tool

--- packages/core/src/ai/agents/commonTools.ts
+++ packages/core/src/ai/agents/commonTools.ts
@@ -1,11 +1,38 @@
 import { tool } from "ai";
+import { Resource } from "sst";
 import { z } from "zod";
+import type { BraveSearchResponse } from "../../types/brave";
 
 export const today = tool({
-  description: "Get the current date, use this tool to get the current date and time. You can also use it to calculate future dates like tomorrow, next week, next month, next year, next decade",
-  parameters: z.object({}),
-  execute() {
-    const date = new Date();
-    return Promise.resolve(date.toISOString());
-  }
-})
+	description:
+		"Get the current date, use this tool to get the current date and time. You can also use it to calculate future dates like tomorrow, next week, next month, next year, next decade",
+	parameters: z.object({}),
+	execute() {
+		// Set timezone to PST
+		const date = new Date();
+		date.setHours(date.getHours() - 8);
+		return Promise.resolve(date.toISOString());
+	},
+});
+
+export const webSearch = tool({
+	description: "Search the web for information",
+	parameters: z.object({
+		query: z.string(),
+	}),
+	async execute(params) {
+		const url = new URL("https://api.search.brave.com/res/v1/web/search");
+
+		url.searchParams.set("q", params.query);
+
+		const response = await fetch(url.toString(), {
+			method: "GET",
+			headers: {
+				"X-Subscription-Token": Resource.BRAVE_API_KEY.value,
+			},
+		});
+
+		const results = (await response.json()) as BraveSearchResponse;
+		return results.web?.results || [];
+	},
+});
