1ade8808bb2a09b56e6ede8c1d0fd8f3f55c0761
Michael <michaeljohneduave@gmail.com>
2025-05-09 15:46:48 -0700

    Re-add manifest json

--- apps/chat/vite.config.ts
+++ apps/chat/vite.config.ts
@@ -43,14 +43,14 @@ export default defineConfig({
 				start_url: "/",
 				icons: [
 					{
-						src: "pwa-192x192.png",
+						src: "pwa-192x192.webp",
 						sizes: "192x192",
-						type: "image/png",
+						type: "image/webp",
 					},
 					{
-						src: "pwa-512x512.png",
+						src: "pwa-512x512.webp",
 						sizes: "512x512",
-						type: "image/png",
+						type: "image/webp",
 						purpose: "any maskable",
 					},
 					{
